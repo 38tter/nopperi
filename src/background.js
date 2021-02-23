c753c0636a8188db40f9439ff58761a82595c337
Seiya Miyata <odradek38@gmail.com>
2021-02-24 02:10:10 +0900

    Add fetch and remove actions to background.js

--- src/background.js
+++ src/background.js
@@ -1,3 +1,33 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.sendMessage(tab.id, "clicked");
+chrome.browserAction.onClicked.addListener((tab) => {
+    chrome.tabs.sendMessage(tab.id, "clicked", (res) => {
+        console.log(res);
+    });
 });
+
+chrome.extension.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.message == "fetch") {
+            const url = request.url;
+            (async () => {
+                await tabCreate(url);
+                chrome.tabs.getSelected(null, tab => {
+                    chrome.tabs.sendMessage(tab.id, "subTab", (res) => {
+                        sendResponse(res);
+                    });
+                });
+            })();
+            sendResponse('sendMessageFinished.')
+        }
+        if (request.message == "remove") {
+            chrome.tabs.getSelected(null, tab => {
+                chrome.tabs.remove(tab.id);
+            });
+            sendResponse('removeEnd');
+        }
+        return true;
+    }
+)
+
+const tabCreate = async (url) => {
+    chrome.tabs.create({ url: url });
+}
