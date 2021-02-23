ffec0bb54001a7557559709287021de2d1154e09
Seiya Miyata <odradek38@gmail.com>
2021-02-24 02:10:32 +0900

    Add fetch action to content.js

--- src/content.js
+++ src/content.js
@@ -1,5 +1,30 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request == "clicked") {
-        console.log('clicked');
+        var links = document.getElementsByClassName('notion-page-content');
+        var e = links[0].childNodes;
+        for (var i = 0; i < e.length; ++i) {
+            var a = e[i].getElementsByTagName('a')
+            if (!a.length) {
+                continue;
+            }
+            chrome.runtime.sendMessage(
+                {
+                    message: "fetch",
+                    //a tag は 1 個しか含まれていない（多分）
+                    url: a[0].href
+                },
+                function (res) {
+                    console.log(res);
+                }
+            );
+            chrome.runtime.sendMessage({
+                message: "remove",
+            }, res => console.log(res));
+        }
+        sendResponse({ text: 'clickedResponse' });
     }
+    if (request == "subTab") {
+        sendResponse("subtab");
+    }
+    return true
 });
